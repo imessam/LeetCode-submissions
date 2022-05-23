1cfa970694950fe055b69f0bbea06acd7cafe1bd
Mohamed Essam Abdelfatah <31731234+imessam@users.noreply.github.com>
2022-05-23 20:31:59 +0200

    Time: 27 ms (5.23%), Space: 11.5 MB (49.58%) - LeetHub

---
+++ 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int peakIndexInMountainArray(vector<int>& arr) {
+        int low,high,mid,max;
+        
+        low = 0;
+        high = arr.size()-1;
+        mid = (low+high)/2;
+        max = arr[mid];
+        
+        while(((mid-1) >=0 && max<arr[mid-1]) || ((mid+1) < arr.size() && max < arr[mid+1])){
+            cout<<low<<" , "<<mid<<" , "<<high<<endl;
+            if(((mid-1) >=0 && max<arr[mid-1]))
+                high = mid-1;
+            else
+                low = mid+1;
+            mid = (low+high)/2;
+            max = arr[mid];
+        }
+        return mid;
+    }
+};
