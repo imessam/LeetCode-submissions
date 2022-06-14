938d2db6d9126f5d768f03cc1b5c6bfd871e5de0
Mohamed Essam Abdelfatah <31731234+imessam@users.noreply.github.com>
2022-06-14 21:42:33 +0200

    Time: 17934 ms (5.01%), Space: 27.4 MB (6.19%) - LeetHub

---
+++ 45-jump-game-ii/45-jump-game-ii.py
@@ -0,0 +1,42 @@
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        
+        n = len(nums)
+        count = 0
+        dp = [False]*n
+        
+        dp[n-1] = True
+        
+        leastBestIdx = n-1
+        
+        
+        for i in range(n-2,-1,-1):
+            
+            if i+nums[i] >= leastBestIdx:
+                leastBestIdx = i
+                dp[i] = True
+        print(dp)        
+        @cache
+        def minJumps(sr):
+            
+            if sr >= (n-1):
+                return 0
+
+            minJump = n
+
+            for i in range(nums[sr],0,-1):
+
+                if (sr+i) < len(dp) and dp[sr+i] == True:
+                    minJump = min(minJump,minJumps(sr+i))
+
+            return 1+minJump
+        
+        return minJumps(0)
+        
+        
+                
+        
+        
+        
+    
+        
