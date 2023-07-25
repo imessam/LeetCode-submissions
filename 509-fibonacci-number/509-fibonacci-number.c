fb1f2b42bba8e1284f679711cdd9e19fb2ae6fe2
Mohamed Essam Abdelfatah <31731234+imessam@users.noreply.github.com>
2023-07-25 22:55:10 +0300

    Time: 8 ms (50.52%), Space: 5.6 MB (10.48%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.c
+++ 509-fibonacci-number/509-fibonacci-number.c
@@ -3,16 +3,5 @@ int fib(int n){
     if (n <= 1)
         return n;
     
-    int F[n+1];
-    
-    for (int i = 0; i<=n; i++)
-        F[i] = 0;
-    
-    F[0] = 0;
-    F[1] = 1;
-    
-    for (int i = 2; i<=n; i++){
-        F[i] = F[i-2] + F[i-1];
-    }
-    return F[n];
+    return fib(n-2) + fib(n-1);
 }
